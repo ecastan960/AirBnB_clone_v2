8434d6b14e9b1d4c574af3c20736289f26d4dee6
ecastan960 <ecastan1@eafit.edu.co>
2021-04-16 10:50:50 -0500

    09|Update file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,17 +25,17 @@ def do_deploy(archive_path):
     name_file = fileP[1].split('.')
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(name_file[0]))
-        run("tar -xzf /tmp/{} -C\
+        sudo("mkdir -p /data/web_static/releases/{}".format(name_file[0]))
+        sudo("tar -xzf /tmp/{} -C\
             /data/web_static/releases/{}".format(fileP[1], name_file[0]))
-        run("rm -rf /tmp/{}".format(fileP[1]))
-        run("mv -n /data/web_static/releases/{}/web_static/* \
+        sudo("rm -rf /tmp/{}".format(fileP[1]))
+        sudo("mv -n /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(name_file[0], name_file[0]))
-        run("rm -rf /data/web_static/releases/{}\
+        sudo("rm -rf /data/web_static/releases/{}\
             /web_static".format(name_file[0]))
-        run("rm -rf /data/web_static/current")
+        sudo("rm -rf /data/web_static/current")
         slink = "ln -s /data/web_static/releases/{} /data/web_static/current"
-        run(slink.format(name_file[0]))
+        sudo(slink.format(name_file[0]))
         return True
     except Exception:
         return False
