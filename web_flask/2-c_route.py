8a82f6b9c2a9cc0c1e82717a6a17f0b7fae19239
ecastan960 <ecastan1@eafit.edu.co>
2021-05-02 10:50:29 -0500

    00|First test

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that starts a flask web app.
+"""
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Route / definition
+
+    Returns:
+        [string]: "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route /hbnb definition
+
+    Returns:
+        [string]: "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """Route /c/<text> definition
+    receives a varible text
+
+    Returns:
+        [string]: "C <text>"
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
