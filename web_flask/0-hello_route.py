a05449e29d1357b93594801cfe01e5a3c1289173
ecastan960 <ecastan1@eafit.edu.co>
2021-05-02 07:25:08 -0500

    01|update route definition

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,9 @@
 from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/, strict_slashes=False")
 def home():
     """Route / definition
 
