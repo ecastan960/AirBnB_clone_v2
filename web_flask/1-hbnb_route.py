d0e659784fa51338fde0c4de90b582f4e5b7171f
ecastan960 <ecastan1@eafit.edu.co>
2021-05-02 08:25:58 -0500

    00|First test

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that starts a flask web app.
+"""
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Route / definition
+
+    Returns:
+        [string]: "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route / definition
+
+    Returns:
+        [string]: "HBNB"
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
